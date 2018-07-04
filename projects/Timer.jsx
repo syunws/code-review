6e49fcceef545cf71635f4bdb448b26d5543e5d2
sooylee <syunws@gmail.com>
2018-07-04 14:36:38 +0900

    expireDate 추가 했습니다

--- projects/Timer.jsx
+++ projects/Timer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import 'moment/locale/ko';
 moment.local = 'ko';
+//moment.relativeTimeThreshold('m', 1440); // 24시간 이내는 분으로 나타내도록
 
 class Timer extends Component {
   constructor(props) {
@@ -16,7 +17,6 @@ class Timer extends Component {
 
   handleTick = () => {
     console.log(1);
-
     this.forceUpdate();
   };
 
@@ -24,7 +24,7 @@ class Timer extends Component {
     return (
       <div>
         <div>현재시간은 :{moment().format('YYYY-MM-DD A hh:mm:ss')} </div>
-        <div>{moment('2018-07-04T17:00:00+0900').fromNow()} 에 종료 합니다. </div>
+        <div>{moment(this.props.expireDate).fromNow()} 에 종료 합니다. </div>
       </div>
     );
   }
